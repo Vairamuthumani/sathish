8fb06fc8717da6b6ca05d47d504c9ff1a69794fd
vairamuthu <vairamuthumani06@gmail.com>
2025-08-05 23:50:57 +0530

    change

---
+++ lap.java
@@ -0,0 +1,14 @@
+public class lap {
+ String name="";
+ int storage=0;
+ String proc="";
+
+
+public static void main(String args[]){
+ lap laptop=new lap();
+ laptop.name="loq";
+ {
+    System.out.println(laptop.name);
+ }
+}
+}
